d85932c305d785d2f838bc5dcdadc665b5a870e3
eonduck2 <whdtnxd@gmail.com>
2024-06-04 11:25:10 +0900

    update class 'WorkTwo'

--- work-2.js
+++ work-2.js
@@ -15,8 +15,25 @@ class WorkTwo {
   get a() {
     return this._a;
   }
+
+  set b(value) {
+    if (!isNaN(value)) {
+      this._b = value;
+    } else {
+      throw new Error(`타입 에러`);
+    }
+  }
+
+  get b() {
+    return this._b;
+  }
+
+  add() {
+    return this._a + this._b;
+  }
 }
 
 const workTwo = new WorkTwo(1, 2);
 
-console.log(workTwo);
+console.dir(workTwo);
+console.log(workTwo instanceof WorkTwo);
