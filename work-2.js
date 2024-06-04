e618a4b4e78d8f37c2c9b641ca65d815311af116
eonduck2 <whdtnxd@gmail.com>
2024-06-04 10:46:07 +0900

    update class 'WorkTwo'

--- work-2.js
+++ work-2.js
@@ -3,6 +3,14 @@ class WorkTwo {
     this.a = a;
     this.b = b;
   }
+
+  set a(value) {
+    this._a = value;
+  }
+
+  get a() {
+    return this._a;
+  }
 }
 
 const workTwo = new WorkTwo(1, 2);
